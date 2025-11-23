21b7f9056f364a09a2b6f82d76c0234a263e5fcc
KhamuKusan7 <83013033+KhamuKusan7@users.noreply.github.com>
2025-11-23 14:30:00 +0545

    add player images

--- src/MyComponents/PlayerProfile.jsx
+++ src/MyComponents/PlayerProfile.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 export default function PlayerProfile() {
   const clubBg = {
-    "Arsenal" : "/images/Club_bg/Arsenal_bg.png",
+    "Arsenal" : "/images/Club_bg/Arsenal_bg1.png",
     "Man City": "/images/Club_bg/ManCity_bg.png",
     "Liverpool": "/images/Club_bg/Liverpool_bg.png",
     "Chelsea" : "/images/Club_bg/Chelsea_bg.png",
     "Man United" : "/images/Club_bg/ManUtd_bg.png",
     "Tottenham" : "/images/Club_bg/Spurs_bg.png",
-    "Newcastle" : "/images/Club_bg/Newcastle_bg.png",
+    "Newcastle" : "/images/Club_bg/Newcastle_bg2.png",
     "Crystal Palace" : "/images/Club_bg/CrystalPalace_bg.png",
     "Everton" : "/images/Club_bg/Everton_bg.png",
     "Bournemouth" : "/images/Club_bg/Bournemouth_bg.png",
     "Nottingham" : "/images/Club_bg/Nottingham_bg.png",
     "Sunderland" : "/images/Club_bg/Sunderland_bg.png",
-    "Aston Villa" : "/images/Club_bg/AstonVilla_bg.png",
+    "Aston Villa" : "/images/Club_bg/AstonVilla_bg2.png",
     "Brentford" : "/images/Club_bg/Brentford_bg.png",
     "Brighton" : "/images/Club_bg/Brighton_bg.png",
     "Fulham" : "/images/Club_bg/Fulham_bg.png",
@@ -27,7 +27,7 @@ export default function PlayerProfile() {
 
   const players = [
     { name: "Mohamed Salah", img: "/images/Players/Salah.png", club: "Liverpool", link:"Salah"},
-    { name: "Viktor Gyökeres", img: "/images/Players/Gyokeres.png", club:"Arsenal"},
+    { name: "Gabriel Magalhaes", img: "/images/Players/GabrielMagalhaes.png", club:"Arsenal"},
     { name: "Erling Haaland", img: "/images/Players/Haaland.png", club: "Man City", link:"Haaland"},
     { name: "Cole Palmer", img:"/images/Players/Palmer.png", club:"Chelsea"},
     { name: "Bruno Fernandes", img:"/images/Players/Bruno_Fernandes.png", club:"Man United"},
@@ -37,7 +37,7 @@ export default function PlayerProfile() {
     { name: "Jack Grealish", img:"/images/Players/Grealish.png", club:"Everton"},
     { name: "Antoine Semenyo", img:"images/Players/Semenyo.png", club:"Bournemouth"},
     { name: "Granit Xhaka", img:"/images/Players/Xhaka.png", club:"Sunderland"},
-    { name: "Morgan Rogers", img:"images/Players/MorganRogers.png", club:"Aston Villa"},
+    { name: "John McGinn", img:"images/Players/John_McGinn1.png", club:"Aston Villa"},
     { name: "Morgan Gibbs-White", img:"images/Players/GibbsWhite.png", club:"Nottingham"},
     { name: "Jordan Henderson", img:"images/Players/JordanHenderson.png", club:"Brentford"},
     { name: "Kaoru Mitoma", img:"images/Players/Mitoma.png", club:"Brighton"},
@@ -45,7 +45,7 @@ export default function PlayerProfile() {
     { name: "Hwang Hee-chan", img:"images/Players/HwangHeeChan.png", club:"Wolves"},
     { name: "Alex Iwobi", img:"images/Players/Iwobi.png", club:"Fulham"},
     { name: "Kyle Walker", img:"images/Players/KyleWalker.png", club:"Burnley"},
-    { name: "Lukas Nmecha", img:"images/Players/Nmecha.png", club:"Leeds"}
+    { name: "Sean Longstaff", img:"images/Players/Sean_Longstaff.webp", club:"Leeds"}
   ];
 
   return (
